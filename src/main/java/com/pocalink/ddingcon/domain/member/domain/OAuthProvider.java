6ac4ff1ca589727a1cff3051feebff33d014a5fb
1 <wrasf175@naver.com>
2023-12-30 23:40:40 +0900

    feat : 임시 용도의 Member Dto 생성

--- src/main/java/com/pocalink/ddingcon/domain/member/domain/OAuthProvider.java
+++ src/main/java/com/pocalink/ddingcon/domain/member/domain/OAuthProvider.java
@@ -1,2 +1,7 @@
-package com.pocalink.ddingcon.domain.member.domain;public enum OAuthProvider {
+package com.pocalink.ddingcon.domain.member.domain;
+
+
+// 로그인 인증 플랫폼 추가를 고려해 ENUM 생성
+public enum OAuthProvider {
+    KAKAO
 }
